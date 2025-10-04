d7e9562ef7dba9e2b77bb1c79d8241824e3c1972
SDM30 <simondiaz@javeriana.edu.co>
2025-10-03 22:02:34 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,169 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+#include"shaderClass.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+// Vertices coordinates
+GLfloat pyramidVertexData[] =
+{ //     COORDINATES     /        COLORS      
+	// Base 
+	-0.5f, 0.0f,  0.5f,     1.0f, 0.0f, 0.0f,	// R (vert 0)
+	-0.5f, 0.0f, -0.5f,     0.0f, 1.0f, 0.0f,	// G (vert 1)
+	 0.5f, 0.0f, -0.5f,     0.0f, 0.0f, 1.0f,	// B (vert 2)
+	 0.5f, 0.0f,  0.5f,     1.0f, 0.0f, 1.0f,	// Dark G(vert 3)
+
+	 // Peak
+	  0.0f, 0.8f,  0.0f,     1.0f, 0.5f, 0.0f	// Orange (vert 4)
+};
+
+
+GLuint pyramidElementIndices[] =
+{
+	0, 1, 2, // Pyramid base upper diagonal
+	0, 2, 3, // Pyramid base lower diagonal
+
+	0, 1, 4, // Side 1 front
+	1, 2, 4, // Side 2 right
+	2, 3, 4, // Side 3 back
+	3, 0, 4  // Side 4 left
+};
+
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(width, height, "PARCIAL II", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, width, height);
+
+	// Generates Shader object using shaders defualt.vert and default.frag
+	Shader shaderProgram("default.vert", "default.frag");
+
+	// Generates Vertex Array Object and binds it
+	VAO VAO1;
+	VAO1.Bind();
+
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(pyramidVertexData, sizeof(pyramidVertexData));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(pyramidElementIndices, sizeof(pyramidElementIndices));
+
+	// Links VBO to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+
+	// Variables that help the rotation of the pyramid
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	// Enables the Depth Buffer
+	glEnable(GL_DEPTH_TEST);
+
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.10f, 0.10f, 0.17f, 1.0f);
+		// Clean the back buffer and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		shaderProgram.Activate();
+
+		// Simple timer
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1.0f / 60.0f)
+		{
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
+
+
+		// transformation matrices
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		// Assigns different transformations to each matrix
+		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.1f, -3.0f));
+		// camera transformation
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width/height), 0.1f, 100.0f);
+;
+
+		// Outputs the matrices into the Vertex Shader
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glPointSize(10.0f);
+		glDrawElements(GL_TRIANGLES, sizeof(pyramidElementIndices)/sizeof(GLuint), GL_UNSIGNED_INT, 0);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+
+	// Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
+}
---
+++ VAO.cpp
@@ -0,0 +1,35 @@
+#include"VAO.h"
+
+// Constructor that generates a VAO ID
+VAO::VAO()
+{
+	glGenVertexArrays(1, &ID);
+}
+
+// Links a VBO Attribute such as a position or color to the VAO
+void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset)
+{
+	VBO.Bind();
+	// This line tells OpenGL how to read the VBO data and send it to the vertex shader
+	glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset);
+	glEnableVertexAttribArray(layout);
+	VBO.Unbind();
+}
+
+// Binds the VAO
+void VAO::Bind()
+{
+	glBindVertexArray(ID);
+}
+
+// Unbinds the VAO
+void VAO::Unbind()
+{
+	glBindVertexArray(0);
+}
+
+// Deletes the VAO
+void VAO::Delete()
+{
+	glDeleteVertexArrays(1, &ID);
+}
